084ff0ec5f5cd4c5069a8b566caad4eb29a6f2f3
zxzxzxspork <zxzxzxspork@gmail.com>
2025-05-05 15:41:21 +0200

    test

--- service/service.js
+++ service/service.js
@@ -31,7 +31,7 @@ const apps = {
                     [
                         new tizen.ApplicationControlData("module", [JSON.stringify(
                             {
-                                moduleName: '@foxreis/tizentube',
+                                moduleName: '@kaczy/tizentubezx',
                                 moduleType: 'npm',
                                 args: launchData
                             }
